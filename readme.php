f28ea409322c4abb50450ca6a597706d646980fe
timeblocker <support@timeblocker.com>
2014-06-29 00:39:44 -0400

    Update readme.php

--- readme.php
+++ readme.php
@@ -1,1 +1,21 @@
 # Timeblocker PHP SDK
+
+### Overview
+
+This is the officially supported development kit for PHP to interface with [Timeblocker](http://timeblocker.co/ "Timeblocker"). This library takes a couple of design principles from Backbone.js and Laravel and combine them together to create a full REST client for [Timeblocker](http://timeblocker.co/ "Timeblocker"). Timeblocker has over 100 endpoints for developers, and this library interacts will all them.
+
+### Getting Started
+
+To get started using the API and SDK, use the [Getting Started guide](http://timeblocker.dev/docs/api/getting-started "Getting Started"). For the full reference of endpoints, refer to the [API Documentation](http://timeblocker.co/docs/api/v1 "API Documentation").
+
+### Installation
+
+To install this library, it's highly recommended you use [Composer](https://getcomposer.org/ "Composer").
+
+    "require": {
+        "timeblocker/timeblocker": "0.9.*"
+    }
+
+### Development Status
+
+Timeblocker API is currently in beta. We are stabilizing the platform and trying to ensure consistency and stability with all the endpoints. We hope to have as minimal breaking changes as possible before the 1.0 release. If you need to use the Timeblocker API in production, we would love to [hear from you](http://timeblocker.co/support/ "Contact Us")
